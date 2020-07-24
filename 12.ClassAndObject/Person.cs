06e342233e66753d17f576c5f819c62092a8aace
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 21:57:11 +0600

    Add files via upload

---
+++ 12.ClassAndObject/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+
+        public void Information()
+        {
+            Console.WriteLine("Hi, my name is {0} {1} and I am {2} years old!", firstName,lastName,age) ;
+        }
+    }
+}
---
+++ 12.ClassAndObject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Zakir = new Person();
+            Zakir.firstName = "Zakir";
+            Zakir.lastName = "Hossain";
+            Zakir.age = 25;
+            Zakir.Information();
+
+            Person Rakib = new Person()
+            {
+                firstName = "Rakib Rahman",
+                lastName="Talukder",
+                age=23
+            };
+
+            Rakib.Information();
+
+            Person Shuvo = new Person()
+            {   
+                firstName="Ashraful Alam",
+                lastName="Shuvo",
+                age=35,
+               
+            };
+            Shuvo.Information();
+
+            Person Zahid = new Person()
+            {
+                firstName="Zahid",
+                lastName="Hasan",
+                age=25
+            };
+            Zahid.Information();
+        }
+    }
+}
