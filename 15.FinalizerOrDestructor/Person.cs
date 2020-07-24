cdae17ab3ff0346e4890732184482a80d538a841
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:00:44 +0600

    Add files via upload

---
+++ 15.FinalizerOrDestructor/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalizerOrDestructor
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("My name is {0} {1} and I am {2} years old!",firstName,lastName,age);
+        }
+
+        ~Person()
+        {
+            Console.WriteLine("Finalizer or Destructor Calling");
+            
+        }
+    }
+}
---
+++ 15.FinalizerOrDestructor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalizerOrDestructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person zakir = new Person("Zakir", "Hossain", 25);
+            zakir.Info();
+            Console.ReadKey();
+        }
+    }
+}
