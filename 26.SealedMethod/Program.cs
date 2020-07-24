69c90e8f436d986d3edaf46289c17dd3dec965a3
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:15:05 +0600

    Add files via upload

---
+++ 26.SealedMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedMethod
+{
+    class Person
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("This is From Person Class");
+        }
+    }
+    class Teacher : Person
+    {
+        public sealed override void Display()
+        {
+            base.Display();
+            Console.WriteLine("This is From Teacher Class");
+        }
+    }
+    //class Student : Teacher
+    //{
+    //    public override void Display()
+    //    {
+    //        base.Display();
+    //        Console.WriteLine("This is From Student Class");
+    //    }
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student std = new Student();
+            //std.Display();
+        }
+    }
+}
