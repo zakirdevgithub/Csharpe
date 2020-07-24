e62be422c138abf0859c421f41316684c40eddaa
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:27:47 +0600

    Add files via upload

---
+++ 34.GenericMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethod
+{   
+    class Calculation 
+    {
+
+        public void Add <T>(T a , T b)
+        {
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation calc = new Calculation();
+            calc.Add(20,30);
+            calc.Add("Zakir", "Hossain");
+        }
+    }
+}
