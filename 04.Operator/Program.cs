5cee9cf5007679b377fb7b6433a41b095f475a01
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 21:47:10 +0600

    Add files via upload

---
+++ 04.Operator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 20;
+            int c = 40;
+            Console.WriteLine("20 + 10 ="+ (b + a));
+            Console.WriteLine("20 - 10 ="+ (b - a));
+            Console.WriteLine("20 * 10 ="+ (b * a));
+            Console.WriteLine("20 / 10 ="+ (b / a));
+            Console.WriteLine("20 % 10 ="+ (b % a));
+            Console.WriteLine("20 > 10 ="+ (b > a));
+            Console.WriteLine("20 < 10 ="+ (b < a));
+            Console.WriteLine("20 >= 10 ="+ (b >= a));
+            Console.WriteLine("20 <= 10 ="+ (b <= a));
+            Console.WriteLine("20 != 10 ="+ (b != a));
+            Console.WriteLine("10 > 20 and 20 < 40 is "+(a>b && b<c));
+            Console.WriteLine("10 > 20 or 20 < 40 is "+(a>b || b<c));
+
+        }
+    }
+}
