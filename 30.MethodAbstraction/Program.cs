26fd1de4af6d1c5962610925597f28759907cd2e
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:19:09 +0600

    Add files via upload

---
+++ 30.MethodAbstraction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAbstraction
+{
+    class Person
+    {
+        private void sensitiveInfo()
+        {
+            Console.WriteLine("This is Sentitive Information so you cant call this method directly");
+        }
+
+        public void showSecret()
+        {
+            sensitiveInfo();
+            Console.WriteLine("Sensitive info showed");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person pr = new Person();
+            pr.showSecret();
+        }
+    }
+}
