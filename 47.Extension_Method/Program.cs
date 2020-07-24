388606179da6bd0dc3ea0a0ea8e2232aff1029f7
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:57:23 +0600

    Add files via upload

---
+++ 47.Extension_Method/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            bool result = i.IsGreaterThan(100);
+            Console.WriteLine(result);
+        }
+
+       
+    }
+    public static class MyExtensionMethod
+    {
+        public static bool IsGreaterThan(this int i, int number)
+        {
+            return i > number;
+        }
+    }
+  
+}
