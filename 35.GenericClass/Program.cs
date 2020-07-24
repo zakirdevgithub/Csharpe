373a1d2dd91e16c9e0fe3ba9534b189337cb2116
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:29:30 +0600

    Add files via upload

---
+++ 35.GenericClass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    class Person<T>
+    {
+        private T name;
+        private T age;
+
+        public T Name
+        {
+            set => this.name = value;
+            get => this.name;
+        }
+        public T Age
+        {
+            set => this.age = value;
+            get => this.age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person<string> zakir = new Person<string>();
+            zakir.Name = "Zakir Hossain";
+            zakir.Age = "25";
+            Console.WriteLine(zakir.Name);
+            Console.WriteLine(zakir.Age);
+        }
+    }
+}
