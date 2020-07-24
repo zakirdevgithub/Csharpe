fcfe9170363621f08c75412cbbdd4f1de778fb67
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 21:45:09 +0600

    Add files via upload

---
+++ 02.PrimitiveDataType/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 255;
+            Console.WriteLine( b);
+            short s = 4567;
+            Console.WriteLine(s);
+            int num = 747548;
+            Console.WriteLine(num);
+            float pi = 3.1416f;
+            Console.WriteLine(pi);
+            double weight = 67.9;
+            Console.WriteLine(weight);
+            decimal total = 786.9867m;
+            Console.WriteLine(total);
+            bool isWorking = true;
+            Console.WriteLine(isWorking);
+            char firstChar = 'A';
+            Console.WriteLine(firstChar);
+            string name = "Zakir Hossain";
+            Console.WriteLine(name);
+            var age = 25;
+            Console.WriteLine(age);
+            var friend = "Shovon";
+            Console.WriteLine(friend);
+        }
+    }
+}
