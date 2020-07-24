4184eef31547105e1c83ab1191e1f4ebce429b19
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:07:01 +0600

    Add files via upload

---
+++ 19.PassByRefferenceOUT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassByRefferenceOUT
+{
+    class Program
+    {
+        static void PassByRefference(out int x)
+        {
+            x = 20;
+            Console.WriteLine(x);
+        }
+        static void Main(string[] args)
+        {
+            //int value = 5; initialize korleo hobe na korleo hobe.
+            int value; 
+            PassByRefference(out value);
+            Console.WriteLine(value);
+        }
+    }
+}
