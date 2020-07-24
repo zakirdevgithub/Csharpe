7c93baa9abfc2cc749a96ea7b924c5b77edbb356
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:20:16 +0600

    Add files via upload

---
+++ 31.AbstractClass/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    abstract class Person
+    {
+        protected  string name;
+        protected int age;
+        public abstract void Show(string name, int age);
+    }
+    class Student : Person
+    {
+        public override void Show(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std = new Student();
+            std.Show("Zakir Hossain", 25);
+            
+        }
+    }
+}
