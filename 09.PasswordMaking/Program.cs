2b4454a11acb51e865acd8700f9a69fa7a7ec100
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 21:54:26 +0600

    Add files via upload

---
+++ 09.PasswordMaking/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordMaking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int passwordLength = 10;
+            char[] buffer = new char[passwordLength];
+            for(int i=0; i<passwordLength; i++)
+            {
+                buffer[i] = (char)('a' + random.Next(0, 26));
+               
+            }
+            string password = new string(buffer);
+            Console.WriteLine(password);
+        
+            
+        }
+    }
+}
