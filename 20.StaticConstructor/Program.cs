0b32c5bf07bda69c1201fcbe94b9482c6cdbd67c
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:07:53 +0600

    Add files via upload

---
+++ 20.StaticConstructor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticConstructor
+{
+    class Student
+    {
+        public static string firstName;
+        public static string lastName;
+        public static int age;
+
+        static Student()
+        {
+            firstName = "Zakir";
+            lastName = "Hossain";
+            age = 25;
+            Console.WriteLine("My name is {0} {1} and I am {2} years Old!",firstName,lastName,age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student zakir = new Student();
+
+        }
+    }
+}
