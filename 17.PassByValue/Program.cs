442431b9312375cb7137cb0ea1bfb7fa2ac41c58
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:05:09 +0600

    Add files via upload

---
+++ 17.PassByValue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassByValue
+{
+    class Program
+    {
+        static void PassByValue(int x)
+        {
+            x = 20;
+            Console.WriteLine(x);
+        }
+        static void Main(string[] args)
+        {
+            int value = 5;
+            PassByValue(value);
+            Console.WriteLine(value);
+
+
+        }
+    }
+}
