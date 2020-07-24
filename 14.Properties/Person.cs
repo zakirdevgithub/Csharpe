40ed259471b352754c7c1bf126654fd425a4b973
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 21:59:28 +0600

    Add files via upload

---
+++ 14.Properties/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Person
+    {
+        //private string firstName;
+        private string lastName;
+        private int age;
+        private double height;
+
+       public string FirstName { get; set; }
+
+       public string LastName
+        {
+            get => lastName;
+            set => lastName = value;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 20 && value > 12) throw new Exception("You are a Teenager!");
+                age = value;
+            }
+        }
+
+        public double GetHeight()
+        {
+            return this.height;
+        }
+
+        public void SetHeight(double height)
+        {
+            this.height = height;
+        }
+
+        public void Information()
+        {
+            Console.WriteLine("My name is {0} {1} and I am {2} years old and my height is {3}",
+                FirstName,LastName,Age,GetHeight());
+        }
+    }
+}
---
+++ 14.Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person zakir = new Person();
+            zakir.FirstName = "Zakir";
+            zakir.LastName = "Hossain";
+            zakir.Age = 25;
+            zakir.SetHeight(5.7);
+            zakir.Information();
+
+        }
+    }
+}
