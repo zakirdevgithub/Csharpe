c8f0b560333446bfadcddc0278cc2aba2a43fb1f
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:38:34 +0600

    Add files via upload

---
+++ 39.LambdaExpression/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+            List<int> evenNumber = list.FindAll(i => i % 2 == 0);
+            evenNumber.ForEach(i =>
+            {
+                Console.WriteLine(i);
+            });
+        }
+    }
+}
