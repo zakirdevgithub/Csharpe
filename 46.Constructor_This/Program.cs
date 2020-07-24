538e553bffc100143487e67a6be039ef836808c0
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:55:56 +0600

    Add files via upload

---
+++ 46.Constructor_This/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_This
+{
+    public class Person
+    {
+        private string _name;
+        private int _age;
+        
+        public Person()
+        {
+            Console.WriteLine("My Name is Zakir Hossain");
+        }
+
+        public Person(string _name, int _age) : this()
+        {
+            this._name = _name;
+            this._age = _age;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("My name is {0} and I am {1} years old! ",this._name,this._age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Zakir Hossain",25);
+
+            person.Show();
+           
+        }
+    }
+}
