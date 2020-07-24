090a1a83eae7fc8ce98009e8639897dadab78548
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 21:55:22 +0600

    Add files via upload

---
+++ 10.UserInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
