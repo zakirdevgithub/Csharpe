9ece284742670cd461796c777dc2178c40b77eef
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:06:09 +0600

    Add files via upload

---
+++ 18.PassByRefferenceREF/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassByRefferenceREF
+{
+    class Program
+    {
+        static void PassByRefference(ref int x)
+        {
+            x = 20;
+            Console.WriteLine(x);
+        }
+        static void Main(string[] args)
+        {
+            int value = 5;
+            PassByRefference(ref value);
+            Console.WriteLine(value);
+
+        }
+    }
+}
