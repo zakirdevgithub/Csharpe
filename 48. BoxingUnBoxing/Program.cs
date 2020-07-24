9dff1509c4f352a2074e1eea6d95ac2de9ae0802
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 22:59:13 +0600

    Add files via upload

---
+++ 48. BoxingUnBoxing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _51._BoxingUnBoxing
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            object obj = number;  //Boxing
+            
+            Console.WriteLine(obj);
+            
+            number = (int)(obj); //UnBoxing
+
+            Console.WriteLine(number);
+        }
+    }
+}
