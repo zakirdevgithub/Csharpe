1cecce3a49665f28c5b3983b040e1105d5402ac1
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 21:56:07 +0600

    Add files via upload

---
+++ 11.ExceptionHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter First Number:");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Second Number:");
+                int num2 = int.Parse(Console.ReadLine());
+                double result = num1 / num2;
+                Console.WriteLine("Result: "+result);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Divided by is Not Possible");
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for try this");
+            }
+        }
+    }
+}
