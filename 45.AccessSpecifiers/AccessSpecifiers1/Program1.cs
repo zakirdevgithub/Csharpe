76d149fd58f929659bd7b8c93abad4c92b448890
Zakir Hossain <68065676+zakirdevgithub@users.noreply.github.com>
2020-07-24 23:03:43 +0600

    Add files via upload

---
+++ 45.AccessSpecifiers/AccessSpecifiers1/Program1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AccessSpecifiers1
+{  public class Person
+    {
+        public string name;
+        public int age;
+        public double height;
+        public void Display()
+        {
+            Console.WriteLine("Name: {0} Age: {1} Height: {2}", this.name, this.age, this.height);
+        }
+    }
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
